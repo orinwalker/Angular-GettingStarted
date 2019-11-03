44245f7b8e271f24562fa0ca47b3d5fd33f57b0d
Orin Walker <orin.walker@gmail.com>
2019-11-03 08:10:53 -0500

    WIP - Working on Product List Component

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,22 @@
+// import { Component } from '@angular/core';
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>
+  '`
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle = 'Acme Product Mangement';
 }
