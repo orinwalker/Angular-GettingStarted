55c697785b7e890a243f9d52dbe8debc4c3a3048
Orin Walker <orin.walker@gmail.com>
2019-11-16 13:00:10 -0500

    Finished the HTTP Service Integration

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
     // listFilter: string = 'cart';
 
     _listFilter: string;
@@ -48,8 +49,12 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log ('inside OnInit');
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
     }
-
 }
